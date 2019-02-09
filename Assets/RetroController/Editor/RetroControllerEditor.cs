2bd070945c271872a0976b2c063579e7a1abbf19
Worksterr <47071449+Worksterr@users.noreply.github.com>
2019-02-08 21:57:56 -0600

    This is what I have
    welp.  prefaps

---
+++ Assets/RetroController/Editor/RetroControllerEditor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+using UnityEngine;
+using vnc.Editor.Utils;
+
+namespace vnc.Editor
+{
+    [CustomEditor(typeof(RetroController))]
+    public class RetroControllerEditor : UnityEditor.Editor
+    {
+        SerializedProperty profile;
+        SerializedProperty view;
+
+        private void OnEnable()
+        {
+            profile = serializedObject.FindProperty("Profile");
+            view = serializedObject.FindProperty("controllerView");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.Space();
+
+            if (profile.objectReferenceValue == null)
+                EditorGUILayout.HelpBox("The controller doesn't have a Profile attached and won't work.", MessageType.Warning);
+            else
+            {
+                if (GUILayout.Button("Open Profile"))
+                {
+                    //EditorGUIUtility.PingObject(profile.objectReferenceValue);
+                    Selection.activeObject = profile.objectReferenceValue;
+                }
+            }
+
+            if (view.objectReferenceValue == null)
+                EditorGUILayout.HelpBox("The controller doesn't have a view object attached and won't work.", MessageType.Warning);
+
+            EditorUtils.SetIcon(serializedObject.targetObject, "retro_controller");
+
+            DrawDefaultInspectorWithoutScriptField();
+        }
+
+        public bool DrawDefaultInspectorWithoutScriptField()
+        {
+            EditorGUI.BeginChangeCheck();
+
+            serializedObject.Update();
+
+            SerializedProperty Iterator = serializedObject.GetIterator();
+
+            Iterator.NextVisible(true);
+
+            while (Iterator.NextVisible(false))
+            {
+                EditorGUILayout.PropertyField(Iterator, true);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+
+            return (EditorGUI.EndChangeCheck());
+        }
+    }
+
+}
